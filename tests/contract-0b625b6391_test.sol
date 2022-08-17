acda55549ed59bf3c6a583fd7c9f4d675397a403
Remix Workspace <>
2022-08-17 14:30:07 -0300

    .

---
+++ tests/contract-0b625b6391_test.sol
@@ -0,0 +1,13 @@
+// SPDX-License-Identifier: GPL-3.0
+        
+pragma solidity >=0.4.22 <0.9.0;
+
+
+
+import "truffle/Assert.sol";
+import "truffle/DeployedAddresses.sol";
+import "../contracts/contract-0b625b6391.sol";
+// File name has to end with '_test.sol', this file can contain more than one testSuite contracts
+contract testProyecto {
+
+}
